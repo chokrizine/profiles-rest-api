d736e8d80a2735f1856f4175fe99bb018ed2b0ed
chokrizine <zinelabidine.chokri@gmail.com>
2021-01-04 16:04:44 +0100

    added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,4 +7,5 @@ router.register('hello-viewset',views.HelloViewSet,base_name='HelloViewSet')
 router.register('profile',views.UserProfileViewSet)
 urlpatterns=[
 path('hello-view/',views.HelloApiView.as_view()),
+path('login',views.UserLoginApiView.as_view()),
 path('',include(router.urls))]
